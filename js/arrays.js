335a2b50364ab0a008e054f9c67cd25af87ecbe4
favour-uchechukwu <okepikin0882@gmail.com>
2024-05-05 19:07:59 +0100

    Just a little project

---
+++ js/arrays.js
@@ -0,0 +1,26 @@
+// let btn = document.getElementById("getBtn");
+// let url = ;
+
+// btn.addEventListener("click", () => getFunction(url));
+
+// function getFunction(url) {
+//   let fetchPromise = fetch(url);
+//   fetchPromise.then((res) => res.json()).then((data) => renderFunction(data));
+// }
+
+// function renderFunction(datas) {
+//   let container = document.querySelector(".main-container");
+  
+//   datas.forEach((data) => {
+//     container.innerHTML += `
+//     <article key="${data.id}">
+//       <h3>${data.name}</h3>
+//       <div class="left">
+//         <p>${data.username}</p>
+//         <p class="small">${data.email}</p>
+//       </div>
+//     </article>
+//     `
+//   });
+
+
---
+++ js/main.js
@@ -0,0 +1,28 @@
+// class Vue {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greetings() {
+//     if (this.age >= 18) {
+//       console.log(
+//         `Hello ${this.name}, I know that you are ${this.name} years old which makes you eligible to do this`
+//       );
+//     } else {
+//       let year = 18 - this.age;
+//       console.log(
+//         `Hello ${this.name}, please come back in ${year} ${
+//           year == 1 ? "year" : "years"
+//         } time to be eligible to do this`
+//       );
+//     }
+//   }
+// }
+// let myObj = new Vue("Uche", 16);
+// // myObj.greetings();
+
+// const myDetails = {name: "Okenwa",};
+// const a = { age: 45 };
+// const b = { car: "Toyota" };
+// const clone = Object.assign({}, myDetails, a, b);
+// console.log(clone);
